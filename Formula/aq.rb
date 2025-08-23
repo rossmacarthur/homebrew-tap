98d7f5f5d1ac9d9fb37210a4e4cb98d30d850500
Ross MacArthur <17109887+rossmacarthur@users.noreply.github.com>
2025-08-23 17:18:42 +0000

    aq: add 0.3.0 bottle.

--- Formula/aq.rb
+++ Formula/aq.rb
@@ -6,6 +6,14 @@ class Aq < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/rossmacarthur/aq.git", branch: "trunk"
 
+  bottle do
+    root_url "https://github.com/rossmacarthur/homebrew-tap/releases/download/aq-0.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "29c01120ccee5cb2ee541e6ad3354f00a32e29f58d9fdde9e9dab2d0d34c251e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "277f5185da14b404f3077eb43a8972e7f501e995fd278e9569b3db620dca5084"
+    sha256 cellar: :any_skip_relocation, ventura:       "1c156d29f8f9ecd8b4463f1661fef0ac71efb0348377eb26d6640d29a4569fa8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa9f9153394f8ec501ef8a8956cd25bab276c9b3e379f7612c3335114bb0608c"
+  end
+
   depends_on "rust" => :build
   depends_on "jq"
 
